1cc9733ef7fc1a44e013bc73b430e396360ec597
AymanIsmaeel <ayman9089@gmail.com>
2021-01-17 17:45:22 +0100

    //

--- src/main/java/com/globomatics/bike/Models/Bike.java
+++ src/main/java/com/globomatics/bike/Models/Bike.java
@@ -1,18 +1,39 @@
 package com.globomatics.bike.Models;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Entity   // annotate the class as JPA entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) // solve the problem with fetch one session per id, you do not have to serialize this (ignore)
 public class Bike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String email;
     private String phone;
     private String serialNumber;
     private BigDecimal purchasePrice;
+
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern = "dd-mm-yyyy")  // to present the date in a date form
     private Date purchaseDate;
     private boolean contact;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {   // adding a setter is bad because it will be generated automatically but because of jackson lib
+        this.id = id;
+    }
     public String getName() {
         return name;
     }
